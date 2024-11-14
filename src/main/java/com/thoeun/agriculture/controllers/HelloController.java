4b37ffd73d4020a8db16e398e0d304046d008a17
rabunthoeun7777 <rabunthoeun7777@gmail.com>
2024-11-14 09:06:13 +0700

    update

--- src/main/java/com/thoeun/agriculture/controllers/HelloController.java
+++ src/main/java/com/thoeun/agriculture/controllers/HelloController.java
@@ -2,12 +2,15 @@ package com.thoeun.agriculture.controllers;
 
 import com.thoeun.agriculture.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping()
 public class HelloController {
+
+    @GetMapping
     public ResponseEntity<ApiResponse> hello(){
         return ResponseEntity.ok(new ApiResponse("Hello Welcome Agriculture api", null));
     }
